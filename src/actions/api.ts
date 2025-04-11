30ecac0ad0b6c0a6573b9bdf814cd542c54aa1ea
AadityaMall <aadityarmall@gmail.com>
2025-04-12 03:24:26 +0530

    Dockerize + CI/CD Pipeline

--- src/actions/api.ts
+++ src/actions/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:8080", // Update with your backend URL
+  baseURL: process.env.NEXT_PUBLIC_API_URL, // Update with your backend URL
 });
 
 // Attach Authorization Header Automatically
